deee7ad3635f0da5b45df2c2d4171d45b61c781b
LuuizFeelipe <110106741+LuuizFeelipe@users.noreply.github.com>
2023-07-03 20:35:32 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 AOS.init();
+
+//BOTÃO TOPO
+const btn_top = document.getElementById('btn-top');
+function scrollTop (){
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+}
+function scrollBlockNone (){
+    const scrollY = window.scrollY;
+    
+    if(scrollY > 200){
+        btn_top.style.display = "block";
+    } else {
+        btn_top.style.display = "none"
+    }
+}
+btn_top.addEventListener("click", scrollTop);
+window.addEventListener("scroll", scrollBlockNone);
+
