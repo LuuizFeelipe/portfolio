82c871a3bafb014ab77f020d6b7edf68439cae58
LuuizFeelipe <110106741+LuuizFeelipe@users.noreply.github.com>
2023-07-04 23:35:47 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -24,15 +24,31 @@ window.addEventListener("scroll", scrollBlockNone);
 const openNav = document.getElementById('header__menu');
 const closeNav = document.getElementById('close__nav-bar');
 
+// ABRIR MENU
 function openMenu (){
     const navBar = document.getElementById('navbar');
+    const containerNavBar = document.getElementById('container__navbar');
     navBar.style.display = "block";
 }
 
+//FECHAR MENU
 function closeMenu (){
     const navBar = document.getElementById('navbar');
     navBar.style.display = "none";
 }
 
+//DESABILITAR SCROLL
+function disableScroll (){
+    const positionScroll = window.scrollY;
+    document.body.style.overflow = "hidden";
+    window.scrollTo(0, positionScroll);
+}
+
+function enableScroll (){
+    document.body.style.overflow = "auto";
+}
+
 openNav.addEventListener("click", openMenu);
+openNav.addEventListener("click", disableScroll);
 closeNav.addEventListener("click", closeMenu);
+closeNav.addEventListener("click", enableScroll);
